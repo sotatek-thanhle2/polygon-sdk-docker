42a8192f6a31ac7c75828a49c377e5f59532c7c9
Thanh Le <82352098+sotatek-thanhle2@users.noreply.github.com>
2023-05-31 17:17:25 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,12 +1,11 @@
 NUMBER_NODES="${1:-4}"
-PREFIX_NAME_CHAIN="${2:-"test-chain"}"
+PREFIX_NAME_CHAIN="${2:-"prod-chain"}"
 PREMINE_WALLET="${3:-"0x2e355567c6C7127219C75868CCe4C5AD9A2F57f1"}"
 PREMINE_AMOUNT="${4:-10000000000000000000000}"
 
 ARRAY_NUMBER_NODES=()
 NAME_NODES=()
-IP_NODES=()
-DOCKER_NETWORK=polygon-sdk
+DOCKER_NETWORK=polygon-sdk-prod
 
 if [ "$NUMBER_NODES" -lt 4 ]; then
     echo " => Error: Numbers Node should be >= to 4"
@@ -16,7 +15,6 @@ fi
 for ((i=1; i<=$NUMBER_NODES; i++)); do
     ARRAY_NUMBER_NODES+=("$i")
     NAME_NODES+=("$PREFIX_NAME_CHAIN-$i")
-    IP_NODES+=("172.28.0."$i)
 done
 
 # install wget for ubuntu
@@ -24,6 +22,7 @@ apt-get update && apt-get install -y wget
 
 # download polygon-edge 0.9.0
 wget https://github.com/0xPolygon/polygon-edge/releases/download/v0.9.0/polygon-edge_0.9.0_linux_amd64.tar.gz && tar -xzf polygon-edge_0.9.0_linux_amd64.tar.gz
+# cp ~/3rd/polygon-edge_0.9.0_linux_amd64.tar.gz ~/3rd/polygon-sdk-prod && tar -xzf polygon-edge_0.9.0_linux_amd64.tar.gz
 
 # Run and get NodeId of Node
 for i in "${ARRAY_NUMBER_NODES[@]}"; do
@@ -37,9 +36,10 @@ done
 
 # clear network if it already exists
 docker network rm $DOCKER_NETWORK
+docker system prune
 
 # Create network
-docker network create $DOCKER_NETWORK --ip-range=172.28.0.0/24 --driver=bridge --subnet=172.28.0.0/16 --gateway=172.28.5.254
+docker network create $DOCKER_NETWORK
 
 # Remove genesis.json file if it already exists
 rm -rf genesis.json
@@ -47,8 +47,8 @@ rm -rf genesis.json
 # Create a new genesis.json file
 ./polygon-edge genesis --consensus ibft \
     --ibft-validators-prefix-path $PREFIX_NAME_CHAIN- \
-    --bootnode /ip4/${IP_NODES[0]}/tcp/1478/p2p/$(cat ${NAME_NODES[0]}.txt | awk -F "=" {'print $2'} | tail -2 | awk '{gsub(" ", "", $0); print}') \
-    --bootnode /ip4/${IP_NODES[1]}/tcp/1478/p2p/$(cat ${NAME_NODES[1]}.txt | awk -F "=" {'print $2'} | tail -2 | awk '{gsub(" ", "", $0); print}') \
+    --bootnode /dns4/${NAME_NODES[0]}/tcp/1478/p2p/$(cat ${NAME_NODES[0]}.txt | awk -F "=" {'print $2'} | tail -2 | awk '{gsub(" ", "", $0); print}') \
+    --bootnode /dns4/${NAME_NODES[1]}/tcp/1478/p2p/$(cat ${NAME_NODES[1]}.txt | awk -F "=" {'print $2'} | tail -2 | awk '{gsub(" ", "", $0); print}') \
     --premine $PREMINE_WALLET:$PREMINE_AMOUNT
 
 # Build images
@@ -58,10 +58,10 @@ done
 
 # Run images
 for i in "${ARRAY_NUMBER_NODES[@]}"; do
-  docker run -d --name ${NAME_NODES[$((i)) - 1]} -p 0.0.0.0:1100$i:8545 --network $DOCKER_NETWORK --ip ${IP_NODES[$((i)) - 1]} ${NAME_NODES[$((i)) - 1]}
+  docker run -d --name ${NAME_NODES[$((i)) - 1]} -p 0.0.0.0:1110$i:8545 --network $DOCKER_NETWORK ${NAME_NODES[$((i)) - 1]}
 done
 
-# Delete file NodeId
+# # Delete file NodeId
 # for i in "${ARRAY_NUMBER_NODES[@]}"; do
 #   rm ${NAME_NODES[$((i)) - 1]}.txt
 # done
